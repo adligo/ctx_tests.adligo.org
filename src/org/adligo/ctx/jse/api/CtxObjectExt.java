c7ed01d978df48a12cc5e21b38deea11bdd015bf
Scott Morgan <scott@adligo.com>
2022-01-29 20:46:24 -0600

    updating package names

--- src/org/adligo/ctx/jse/api/CtxObjectExt.java
+++ src/org/adligo/ctx/jse/api/CtxObjectExt.java
@@ -1,7 +1,7 @@
 package org.adligo.ctx.jse.api;
 
 import org.adligo.ctx.shared.CtxObject;
-import org.adligo.i.ctx4jse.shared.I_JseCtx;
+import org.adligo.i_ctx4jse.shared.I_JseCtx;
 
 public class CtxObjectExt extends CtxObject {
   public CtxObjectExt(I_JseCtx ctx) {
